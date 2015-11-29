6d745bf16759234792f4c8b4b9d951c8c6b56dd7
Matthew, Chang <matthew.chang@me.com>
2015-11-29 14:08:56 -0800

    upload template

--- pox/web/test.py
+++ pox/web/test.py
@@ -1,9 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+from pox.core import core
+
+import os
+import posixpath
+import urllib
+import cgi
+import errno
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+log = core.getLogger()
+try:
+    weblog = log.getChild("server")
+except:
+    # I'm tired of people running Python 2.6 having problems with this.
+    #TODO: Remove this someday.
+    weblog = core.getLogger("webcore.server")
+
+@app.route('/status')
 def hello_world():
     return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+
+
+def launch ():
+    app.run(debug=TRUE, port=80)
