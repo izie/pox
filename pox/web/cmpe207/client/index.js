9940576540ea004e77e90ec859ddd6b7f52e7300
Matthew, Chang <matthew.chang@me.com>
2015-11-30 12:18:18 -0800

    add lb

---
+++ pox/web/cmpe207/client/index.js
@@ -0,0 +1,9 @@
+var express     = require('express'),
+    app         = express();
+
+/*app.get('/', function(req, res) {
+  res.sendFile('/dist/index.html');
+});*/
+
+
+app.listen(3000,'0.0.0.0');
