2b80c065c3a4eb3322deca4c40e66f1ff9be44ae
Srini Seetharaman <seethara@stanford.edu>
2015-11-30 20:15:03 -0800

    modify client's server

--- pox/web/cmpe207/client/index.js
+++
@@ -1,9 +0,0 @@
-var express     = require('express'),
-    app         = express();
-
-/*app.get('/', function(req, res) {
-  res.sendFile('/dist/index.html');
-});*/
-
-
-app.listen(3000,'0.0.0.0');
