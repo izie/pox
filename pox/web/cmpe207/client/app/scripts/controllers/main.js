1985e67e68614bd78b9d54772d97b215b8309fd2
Matthew, Chang <matthew.chang@me.com>
2015-11-30 20:19:26 -0800

    add lb

--- pox/web/cmpe207/client/app/scripts/controllers/main.js
+++ pox/web/cmpe207/client/app/scripts/controllers/main.js
@@ -9,7 +9,10 @@
  */
 angular.module('clientApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.imageUrl = 'http://10.0.0.254:3000/small';
+    $scope.imageUrl = [];
+    $scope.imageUrl[0] = 'http://10.0.0.254:3000/small';
+    $scope.imageUrl[1] = 'http://10.0.0.254:3000/4k';
+    $scope.imageUrl[2] = 'http://10.0.0.254:3000/5k';
 
     $scope.loadImg = function(val){
       if(val == 'small'){
