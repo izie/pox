83984e65b7045ecb6e144e8199efea434ec8f5e9
Matthew, Chang <matthew.chang@me.com>
2015-12-01 21:11:33 -0800

    add to index.js for client server

---
+++ pox/web/cmpe207/client/index.js
@@ -0,0 +1,18 @@
+// server.js
+
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// configuration =================
+app.use(express.static(__dirname + '/dist'));                 // set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
--- pox/web/cmpe207/server/index.js
+++ pox/web/cmpe207/server/index.js
@@ -9,12 +9,35 @@ app.get('/small', function(req, res) {
   res.sendfile('small.jpg');
 });
 
-app.get('/4k', function(req, res) {
+app.get('/4k',  nocache, sendContent, function(req, res) {
   res.sendfile('5k.jpg');
 });
 
-app.get('/5k', function(req, res) {
+app.get('/5k', nocache, sendContent, function(req, res) {
   res.sendfile('5k_2.jpg');
 });
 
 app.listen(3000,'0.0.0.0');
+
+
+function sendContent(req, res) {
+    var localPath = 'some-file';
+    var mimeType = '';
+    fs.readFile(localPath, 'utf8', function (err, contents) {
+        if (!err && contents) {
+            res.header('Content-Type', mimeType);
+            res.header('Content-Length', contents.length);
+            res.end(contents);
+        } else {
+            res.writeHead(500);
+            res.end();
+        }
+    });
+}
+
+function nocache(req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next();
+}
